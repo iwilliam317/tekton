38d6d1ec28bd0c3e32c5f7c4761b380d88b4d588
is01 <williamobana@internetsistemas.com.br>
2015-03-27 14:57:49 -0300

    criando lista, filtragem inicial por categorias e novidades

--- backend/appengine/routes/produtos/home.py
+++ backend/appengine/routes/produtos/home.py
@@ -5,14 +5,29 @@ from gaecookie.decorator import no_csrf
 from gaepermission.decorator import login_not_required
 
 
-# @login_not_required
-# @no_csrf
-# def index(c=''):
-# 	contexto = {'categoria': c}
-#     return TemplateResponse(contexto,'/produtos/home.html')
-
 @login_not_required
 @no_csrf
-def index(c='tudo'):
-	contexto = {'categoria': c}
+def index(c=''):
+
+	### FIXME ENQUANTO NÃO TEM BD CENTRALIZAR CODIGO ##
+	class Produto (object):
+		def __init__(self, codigo, nome, descricao, preco, categoria, novidades):
+			self.codigo = codigo
+			self.descricao = descricao
+			self.nome = nome
+			self.preco = preco
+			self.categoria = categoria
+			self.novidades = novidades
+
+	produtos = [Produto(1,'Roupa Sapinho', 'Roupa para Cachorro', 59.99, 'roupas',1), Produto(2,'Cama Onça', 'Cama para Cães e Gatos', 89.99, 'camas',0),Produto(3,'Coleira de Couro', 'Coleira para Cães', 59.99, 'coleiras',0),Produto(4,'Roupa Jard', 'Roupa para Cachorro', 79.99, 'roupas',1), Produto(5,'Camiseta Pug', 'Camiseta Unissex', 39.99, 'camisetas',0), Produto(6,'Camiseta Labrador', 'Camiseta Unissex', 39.99, 'camisetas',0), Produto(7,'Pedigree (Petisco)', 'Diversos Sabores', 9.99, 'alimentacao',1), Produto(8,'Ração Pedigree', 'Cachorros até 8kg', 19.99, 'alimentacao',0), Produto(9,'Caneca', 'Mosaico', 7.99, 'canecas',1), Produto(10,'Caneca', 'Chaplin', 7.99, 'canecas',0)]
+
+	if (c!=''):
+		resultados = [x for x in produtos if x.categoria == c]
+
+		if (c=='novidades'):
+			resultados = [x for x in produtos if x.novidades == 1]
+	else:
+		resultados = produtos
+
+	contexto = {'categoria': c, 'produtos' : resultados, 'quantidade': len(resultados)}
 	return TemplateResponse(contexto,'/produtos/home.html')
--- backend/appengine/routes/produtos/produto.py
+++ backend/appengine/routes/produtos/produto.py
@@ -8,6 +8,19 @@ from gaepermission.decorator import login_not_required
 @login_not_required
 @no_csrf
 def index(p='1'):
+
+	# class Produto (object):
+	# 	def __init__(self, codigo, nome, descricao, preco):
+	# 		self.codigo = codigo
+	# 		self.descricao = descricao
+	# 		self.nome = nome
+	# 		self.preco = preco
+
+	# produtos = [Produto(1,'Roupa Sapinho', 'Roupa para Cachorro', 59.90), Produto(2,'Cama Onça', 'Cama para Cães e Gatos', 89.90),Produto(3,'Coleira de Couro', 'Coleira para Cães', 59.90),Produto(4,'Roupa Jard', 'Roupa para Cachorro', 79.90), Produto(4,'Roupa Jard', 'Roupa para Cachorro', 79.90), Produto(1,'Roupa Sapinho', 'Roupa para Cachorro', 59.90),Produto(3,'Coleira de Couro', 'Coleira para Cães', 59.90)]
+
+	# res = filter(lambda x: x.codigo == p, produtos)
+
+
 	contexto= {'produto_id': p}
 	return TemplateResponse(contexto,'/produtos/produto.html')
 
